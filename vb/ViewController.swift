5d0438353817def96a14fe17933fe61dae0a027e
freefreex3 <172932F@mymail.nyp.edu.sg>
2019-07-23 11:37:51 +0800

    Initial Commit

---
+++ vb/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  vb
+//
+//  Created by Justin Tey on 23/07/2019.
+//  Copyright © 2019 NYP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
